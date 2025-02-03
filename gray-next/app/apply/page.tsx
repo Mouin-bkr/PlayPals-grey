537da2dccb3f70db75fe8f9ada2addc399dcda80
mouin <mouinbkr@gmail.com>
2025-02-03 17:20:43 +0100

    form created

---
+++ gray-next/app/apply/page.tsx
@@ -0,0 +1,9 @@
+import JobApplicationForm from "@/components/JobApplicationForm";
+
+export default function ApplyPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <JobApplicationForm />
+    </div>
+  );
+}
